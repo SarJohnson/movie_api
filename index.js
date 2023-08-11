02d67288c86c5a47e768589c5fa0f565f867855d
SarJohnson <sej17b@my.fsu.edu>
2023-08-11 18:08:49 -0400

    Connected to Heroku

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const Users = Models.User;
 const Subgenres = Models.Subgenre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/cfDB', {
+/* mongoose.connect('mongodb://localhost:27017/cfDB', {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true
+}); */
+
+mongoose.connect( process.env.CONNECTION_URI, {
      useNewUrlParser: true, 
      useUnifiedTopology: true
 });
