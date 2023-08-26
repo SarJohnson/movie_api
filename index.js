2b12cd9275f0a1a50fb2310f9edb8f74a514f06c
SarJohnson <sej17b@my.fsu.edu>
2023-08-26 11:57:08 -0400

    Removed authentication from movies endpoint

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to my movie app!');
 });
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
